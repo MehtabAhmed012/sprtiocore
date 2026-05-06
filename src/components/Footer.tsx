821043f70da7dc4367080562098348c6d15337a9
MehtabAhmed012 <105983674+MehtabAhmed012@users.noreply.github.com>
2026-05-06 21:48:21 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -95,8 +95,7 @@ export default function Footer() {
               <li className="flex items-start gap-3">
                 <MapPin size={16} className="text-primary shrink-0 mt-0.5" />
                 <span className="leading-relaxed text-xs">
-                  Street No. 5, Sector J,<br />
-                  Bahria Enclave, Islamabad, Pakistan
+                  Sector J, Bahria Enclave, <br /> Islamabad, Pakistan
                 </span>
               </li>
             </ul>
